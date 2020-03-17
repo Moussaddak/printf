0c4132c7a7462061eb6430d969136abdc9b04842
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-17 00:08:34 +0000

    addingfile

--- test.c
+++ test.c
@@ -1,1 +1,56 @@
-test
+#include <stdarg.h>
+#include "holberton.h"
+/**
+ *_printf - function that produces output according to a format
+ *@format: a string to be displayed in out
+ *Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, flag, k;
+	char special_char  = '%';
+	list lst[] = {
+		{'c', print_char},
+		{'s', print_string}
+	};
+	va_list ap;
+
+	va_start(ap, format);
+
+	while (format && *(format + i))
+	{
+		k = 0;
+		flag = 1;
+
+		if (format[i] == special_char)
+		/* if (format[i] != '%') */
+
+		/* if (!format[i]) */
+			/*->return(i) */
+		{
+			while (k < 2 && flag)
+			{
+				if (format[i+1] == lst[k].c)
+				/* if (!format[i + 1]);) */
+					/*return(-1)*/
+				{
+					flag = 0;
+					lst[k].f(ap);
+					i++;
+				}
+
+
+			/* if (format[i + 1] == %)==> zouze %% ta3tik % wa7da barka*/
+				k++;
+			}
+		}
+		if(flag)
+		{
+			_putchar(format[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+	va_end(ap);
+	return (i);
+}
