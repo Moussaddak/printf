3d769ed70cba7e6c049b9d214e259ba206dfbd63
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-03-19 00:00:56 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #include "holberton.h"
 /**
  *_printf - function that produces output according to a format
@@ -6,45 +5,42 @@
  *Return: the number of characters printed
  */
 int _printf(const char *format, ...)
-{	unsigned int i = 0, flag, k, l = 0, st_len = 0, nb1 = 0, nb2 = 0;
+{	
+  unsigned int i = 0, flag, l = 0, st_len = 0, nb1 = 0, nb2 = 0;
 	char special_char  = '%';
-	list lst[] = {
-		{'c', print_char},
-		{'s', print_string}
-	};
 	va_list ap;
 
 	va_start(ap, format);
 	while (format && *(format + i))
-	{
-		k = 0;
+	{	
 		flag = 1;
-		if (format[i] == special_char &&
-		    format[i + 1] == special_char)
-		{	_putchar('%');
+		if (format[i] == special_char && (format[i + 1] == special_char || format[i + 1] == '\0'))
+		{	
+			_putchar('%');
 			nb1++;
 			flag = 0;
+			i++;
 		}
-		else if (format[i] == special_char)
+		else if (format[i] == special_char && format[i + 1] != '\0'
+		&& get_op_func(format[i + 1]) != NULL)
 		{
-			while (k < 2 && flag)
-			{
-				if (format[i + 1] == lst[k].c)
-				{	nb2++;
-					flag = 0;
-					l = lst[k].f(ap);
-					st_len += l;
-					i++;
-				}
-				k++;
-			}
+			nb2++;
+			flag = 0;
+			l = (get_op_func(format[i + 1]))(ap);
+			st_len += l;
+			i++;
 		}
 		if (flag)
+		{
 			_putchar(format[i]);
-		i++;
+		}
+		if (format[i] != '\0')
+		{
+			i++;
+		}
 	}
 	va_end(ap);
-	if (format == NULL)
+	if (!format)
 		return (-1);
-	return (i - (nb1 - 2 * nb2) + st_len);
+	return (i - (nb1 + 2 * nb2) + st_len);
 }
