d27ba32a5684bfb5f842caa46070387d9392d95a
moussaddak <meddeb.moussaddak@gmail.com>
2020-03-18 21:38:24 +0000

    print string function

---
+++ print_string.c
@@ -0,0 +1,62 @@
+ #include "holberton.h"
+/**
+ *print_char - display a character
+ *@p: argument
+ *Return: number of char
+ */
+int print_char(va_list p)
+{
+	_putchar(va_arg(p, int));
+	return (1);
+}
+/**
+ *print_string - display a string
+ *@p: argument
+ *Return: number of char
+ */
+int print_string(va_list p)
+{
+	char *s;
+	int i;
+
+	s = va_arg(p, char *);
+
+	if (s != NULL)
+	{
+		for (i = 0; s[i]; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+	else
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+
+}
+/**
+ *get_op_func - select the right operation
+ *@s: input a character
+ *Return: number of char
+ */
+int (*get_op_func(char s))(va_list)
+{
+	list lst[] = {
+		{'c', print_char},
+		{'s', print_string}
+	};
+	int i = 0;
+
+
+	while (i < 2)
+	{
+		if (s == lst[i].c)
+		{
+			return (lst[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
