97411d52a766cf66a00cbb999b66ea7664cb5f88
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-03-18 22:47:00 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -1,15 +1,16 @@
+#include "holberton.h"
 #define ABS(x) ((x) < (0) ? (-(x)) : (x))
 /**
  *print_int - print an integer
  *@p: input integer argument
  *Return: n
  */
-int print_int(va_list p)
-{
-	int n, div = 1, i = 0, j, n_i, nb = 0;
-
+ int print_int(va_list p)
+ {
+	int n ,div = 1, i = 0, j, n_i, nb = 0;
+	
 	n = va_arg(p, int);
-
+	
 	if (n < 0)
 	{
 		_putchar('-');
@@ -23,7 +24,7 @@ int print_int(va_list p)
 	}
 	else
 	{
-		for (; n / div >= 10; div *= 10)
+		for(; n / div >= 10; div *=10)
 		{
 			i++;
 		}
@@ -31,15 +32,15 @@ int print_int(va_list p)
 		_putchar((n % 10) + '0');
 		for (j = 1; j <= n_i; j++)
 		{
-			_putchar(((n / _pow(10, j)) % 10) + '0');
+			_putchar(((n / _pow(10,j)) % 10) + '0');
 		}
 		_putchar((n / _pow(10, i)) + '0');
 	}
-
-	return (i + nb);
-
-}
-/**
+	 
+	return (i + nb); 
+	 
+ }
+ /**
  *_pow - returns the value of x raised to the power of y
  *@x: input
  *@y: input
