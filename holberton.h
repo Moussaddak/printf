7f197c865080cf4f7385e24773c4b5b5769c8472
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-03-21 21:05:01 +0100

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define ABS(x) ((x) < (0) ? (-(x)) : (x))
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list p);
